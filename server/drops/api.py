9749c471eca6ac7da6bc1f7a01520a20402ee21a
Mark Neyer <mark@neyer.me>
2014-05-19 23:07:04 -0700

    added new resources, api version and auto creation of api resources

--- server/drops/api.py
+++ server/drops/api.py
@@ -9,3 +9,12 @@ from drops.models import Statement
 class NetworkResource(ModelResource):
     class Meta:
         queryset = Network.objects.all()
+
+class IdentityResource(ModelResource):
+    class Meta:
+        queryset = Identity.objects.all()
+
+
+class StatementResource(ModelResource):
+    class Meta:
+        queryset = Statement.objects.all()
--- server/server/urls.py
+++ server/server/urls.py
@@ -4,16 +4,24 @@ from django.contrib import admin
 from drops import urls as drops_urls
 from drops import api
 
-# implement all the admin urls
-network_resource = api.NetworkResource()
+import inspect
+from tastypie.api import Api
+from tastypie.resources import ModelResource
+
 
 admin.autodiscover()
 
+# create all of the api resources. use reflection instead of hard coding
+# all of those guys in here.
+v1_api = Api(api_name='v1')
+for name, member in inspect.getmembers(api):
+    if inspect.isclass(member) and\
+       ModelResource in inspect.getmro(member) and\
+       member is not ModelResource:
+           v1_api.register(member())
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'server.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^api/', include(network_resource.urls)),
+    url(r'^api/', include(v1_api.urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include(drops_urls)),
 )
